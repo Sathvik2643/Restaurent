3d7a5783b382fd6b6544b41b22f73f940438b9b5
Sathvik2643 <111695889+Sathvik2643@users.noreply.github.com>
2025-12-23 18:49:15 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_AUTH_DOMAIN",
-  databaseURL: "PASTE_DB_URL",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_BUCKET",
-  messagingSenderId: "PASTE_MSG_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyBiergZw0qe1zn8WS9vKRqw0JpV_5AeNjw",
+  authDomain: "restaurent-2643.firebaseapp.com",
+  databaseURL: "https://restaurent-2643-default-rtdb.firebaseio.com",
+  projectId: "restaurent-2643",
+  storageBucket: "restaurent-2643.firebasestorage.app",
+  messagingSenderId: "199318631048",
+  appId: "1:199318631048:web:ebd6114b7110159043a53d",
+  measurementId: "G-T5T11XBEDG"
 };
 
 firebase.initializeApp(firebaseConfig);
