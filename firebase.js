4ede7bec74c964da848596f12ed10a64c7957402
Sathvik2643 <111695889+Sathvik2643@users.noreply.github.com>
2025-12-23 17:54:45 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// js/firebase.js
+
+const firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "PASTE_AUTH_DOMAIN",
+  databaseURL: "PASTE_DB_URL",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_BUCKET",
+  messagingSenderId: "PASTE_MSG_ID",
+  appId: "PASTE_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.database();
