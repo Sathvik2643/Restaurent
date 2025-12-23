74edea1ca8f806696ba1835a5529c5495e452675
Sathvik2643 <111695889+Sathvik2643@users.noreply.github.com>
2025-12-23 17:56:45 +0530

    auth.js

---
+++ auth.js
@@ -0,0 +1,52 @@
+// js/auth.js
+
+let confirmationResult;
+let currentUserUID = null;
+
+// reCAPTCHA
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+  'recaptcha',
+  { size: 'invisible' }
+);
+
+function sendOTP() {
+  const phone = document.getElementById("phone").value;
+
+  auth.signInWithPhoneNumber(phone, window.recaptchaVerifier)
+    .then(result => {
+      confirmationResult = result;
+      document.getElementById("otpSection").style.display = "block";
+    })
+    .catch(err => alert(err.message));
+}
+
+function verifyOTP() {
+  const otp = document.getElementById("otp").value;
+
+  confirmationResult.confirm(otp).then(res => {
+    currentUserUID = res.user.uid;
+
+    db.ref("users/" + currentUserUID).once("value", snap => {
+      if (snap.exists()) {
+        // existing user
+        window.location.href = "waiter.html";
+      } else {
+        // new user
+        document.getElementById("registerSection").style.display = "block";
+      }
+    });
+  }).catch(err => alert("Invalid OTP"));
+}
+
+function completeRegister() {
+  const name = document.getElementById("name").value;
+
+  db.ref("users/" + currentUserUID).set({
+    name: name,
+    role: "waiter",
+    active: true,
+    createdAt: new Date().toISOString()
+  });
+
+  window.location.href = "waiter.html";
+}
